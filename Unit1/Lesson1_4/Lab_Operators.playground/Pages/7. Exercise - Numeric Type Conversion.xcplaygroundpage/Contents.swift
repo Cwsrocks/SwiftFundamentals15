177143bedf4ceb01d7dad167ee0496c23653859c
Caleb Sublett <calebsublett@gmail.com>
2025-01-25 08:13:35 -0500

    Completed operators lab

--- Unit1/Lesson1_4/Lab_Operators.playground/Pages/7. Exercise - Numeric Type Conversion.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_4/Lab_Operators.playground/Pages/7. Exercise - Numeric Type Conversion.xcplaygroundpage/Contents.swift
@@ -3,13 +3,15 @@
 
  Create an integer constant `x` with a value of 10, and a double constant `y` with a value of 3.2. Create a constant `multipliedAsIntegers` equal to `x` times `y`. Does this compile? If not, fix it by converting your `Double` to an `Int` in the mathematical expression. Print the result.
  */
-
-
+let x:Int = 10
+let y:Double = 32
+let multipliedAsIntegers = x * Int(y)
+print(multipliedAsIntegers)
 //:  Create a constant `multipliedAsDoubles` equal to `x` times `y`, but this time convert the `Int` to a `Double` in the expression. Print the result.
-
-
+let multipliedAsDoubles = Double(x) * y
+print(multipliedAsDoubles)
 //:  Are the values of `multipliedAsIntegers` and `multipliedAsDoubles` different? Print a statement to the console explaining why.
-
+print("The values are not different, because they are whole numbers, so the introduction of decimals does not influence them. However, when multiplied as doubles, the result is different, including a vacant decimal place.")
 
 /*:
 [Previous](@previous)  |  page 7 of 8  |  [Next: App Exercise - Converting Types](@next)
