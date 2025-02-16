3b256d891b3e0021928f701196a105edb70ac668
Caleb Sublett <calebsublett@gmail.com>
2025-02-15 21:21:08 -0500

    Completed Functions Lab

--- Unit2/Lesson2_3/Lab_Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_3/Lab_Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
@@ -3,12 +3,38 @@
 
  Write a function called `introduceMyself` that prints a brief introduction of yourself. Call the function and observe the printout.
  */
+func introduceMyself() {
+    print("Hello! My name is Caleb Sublett. It's a pleasure to meet you!")
+}
 
-
+introduceMyself()
 //:  Write a function called `magicEightBall` that generates a random number and then uses either a switch statement or if-else-if statements to print different responses based on the random number generated. `let randomNum = Int.random(in: 0...4)` will generate a random number from 0 to 4, after which you can print different phrases corresponding to the number generated. Call the function multiple times and observe the different printouts.
 import Foundation
+func magicEightBall() {
+    let randomNum = Int.random(in: 0...4)
+    
+    switch randomNum {
+    case 0:
+        print("It is certain")
+    case 1:
+        print("Very doubtful")
+    case 2:
+        print("It is decidedly so")
+    case 3:
+        print("Outlook good")
+    case 4:
+        print("Ask again later")
+    default:
+        break
+    }
+    
+}
 
+magicEightBall()
 
+magicEightBall()
+
+magicEightBall()
 /*:
 page 1 of 6  |  [Next: App Exercise - A Functioning App](@next)
  */
